e02d12c7c1d7ea822ec4a1507514c2c5f3515c14
ramakrishnan <102976005+isolator1@users.noreply.github.com>
2022-11-10 00:53:23 +0530

    Add files via upload

---
+++ StudentManagementWithSpringBootSecurity/src/test/java/com/greatLearning/studentManagement/StudentManagementApplicationTests.java
@@ -0,0 +1,11 @@
+package com.greatLearning.studentManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementApplicationTests {
+
+	
+
+}
