f47510b9ad7a7fc3bf2fc417080336027ce9c3e9
ramakrishnan <102976005+isolator1@users.noreply.github.com>
2022-11-10 20:02:23 +0530

    Add files via upload

---
+++ student_database.sql
@@ -0,0 +1,7 @@
+create database orders_db;
+use `orders_db`;
+employees studentbookcreate table employees(
+id int primary key auto_increment,
+firstName varchar (50),
+lastName varchar (50),
+email varchar (100));
